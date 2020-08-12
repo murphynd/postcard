e91f4a5a4f786fde59dd90c119d693839a56fe46
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-12 11:37:41 -0700

    added new CSS styling for post card to appear
    co-authored-by: Donny Mays <dtmays2@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  $("#formOne").submit(function(){ 
+  $("#formOne").submit(function(event){ 
     const person1Input = $("input#person1").val();
     const adressInput = $("input#adress").val();
 
     $(".person1").append(person1Input);
     $(".adress").append(adressInput);
 
-    $("#story").show();
+    $("#letter").show();
 
     event.preventDefault();
   });
